0c29ff5c978265416f4727d04070f49763a57fb3
yinweiwei198911 <168612555+yinweiwei198911@users.noreply.github.com>
2025-12-09 19:42:23 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -11,33 +11,10 @@ const CUSTOMER_SITES = {
         api:'https://cj.lziapi.com/api.php/provide/vod',
         name:'量子资源',
     },
-    iqy:{
-        api:'https://www.iqiyizyapi.com/api.php/provide/vod',
-        name:'爱奇艺资源',
-    },
-    js:{
-        api:'https://jszyapi.com/api.php/provide/vod',
-        name:'极速资源',
-    },
-    yh:{
-        api:'https://m3u8.apiyhzy.com/api.php/provide/vod',
-        name:'樱花资源',
-    },
-    wj:{
-        api:'https://api.wujinapi.me/api.php/provide/vod',
-        name:'无尽资源',
-    },
-    hh:{
-        api:'https://hhzyapi.com/api.php/provide/vod',
-        name:'豪华资源',
-    },
-    gs:{
-        api:'https://api.guangsuapi.com/api.php/provide/vod',
-        name:'光速资源',
-    },
-    bd:{
-        api:'https://api.apibdzy.com/api.php/provide/vod',
-        name:'百度资源'
+    v0k3:{
+        api:'https://www.xiangjiaozyw.com/api.php/provide/vod',
+        name:'香蕉资源采集接口',
+        adult:true
     }
 };
 
